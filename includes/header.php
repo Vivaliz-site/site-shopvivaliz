be94b03c178292c4c8fbae8250a77f12f676a9a9
Agente Autonomo ShopVivaliz <agente-autonomo@shopvivaliz.com.br>
2026-07-12 03:35:42 -0300

    auto: sincronizar 2026-07-12 03:35:42

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@ $currentPage = basename($_SERVER['PHP_SELF'] ?? '');
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="/css/dazzle-v1.css?v=1.0.0">
+    <link rel="stylesheet" href="/css/dazzle-v1.css?v=1.1.0">
     <style>
         * {
             margin: 0;
