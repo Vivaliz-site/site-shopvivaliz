8f090c858f15db9447e1b6366d6835bf21cc07af
Agente Autonomo ShopVivaliz <agente-autonomo@shopvivaliz.com.br>
2026-07-12 21:07:22 -0300

    auto: sincronizar 2026-07-12 21:07:21

--- auth/login.php
+++ auth/login.php
@@ -274,34 +274,18 @@ $google_auth_url = sv_social_google_auth_url('login', $redirectTo);
 
         <div class="social-buttons">
             <?php if ($google_auth_url !== ''): ?>
-            <a href="<?php echo htmlspecialchars($google_auth_url); ?>" class="btn-social btn-google" title="Continuar com Google">
+            <a href="<?php echo htmlspecialchars($google_auth_url); ?>" class="btn-social btn-google" title="Continuar com Google" style="grid-column: 1 / -1;">
                 <svg viewBox="0 0 24 24" fill="currentColor">
                     <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
                 </svg>
-                Google
+                Continuar com Google
             </a>
             <?php else: ?>
-            <button class="btn-social btn-disabled" disabled title="Google não configurado">
+            <button class="btn-social btn-disabled" disabled title="Google não configurado" style="grid-column: 1 / -1;">
                 <svg viewBox="0 0 24 24" fill="currentColor" style="opacity:0.5">
                     <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
                 </svg>
                 Google
-            </button>
-            <?php endif; ?>
-
-            <?php if ($apple_auth_url !== ''): ?>
-            <a href="<?php echo htmlspecialchars($apple_auth_url); ?>" class="btn-social btn-apple" title="Continuar com Apple">
-                <svg viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M17.05 13.5c-.16-2.74 2.17-4.07 2.26-4.13-.98-1.44-2.51-1.64-3.05-1.66-1.29-.13-2.53.76-3.19.76-.67 0-1.7-.74-2.79-.72-1.44.02-2.77.84-3.51 2.12-1.5 2.6-.38 6.45 1.07 8.56.72 1.04 1.58 2.21 2.71 2.17 1.07-.04 1.48-.69 2.77-.69 1.29 0 1.67.69 2.77.67 1.15-.02 1.88-1.06 2.59-2.1.82-1.19 1.16-2.35 1.18-2.41-.03-.01-2.24-.87-2.27-3.46zM12.03 5.38c.59-.72.98-1.72.87-2.72-.84.03-1.86.56-2.46 1.29-.54.63-1.02 1.63-.89 2.59.94.06 1.9-.47 2.48-1.16z"/>
-                </svg>
-                Apple
-            </a>
-            <?php else: ?>
-            <button class="btn-social btn-disabled" disabled title="Apple não configurado">
-                <svg viewBox="0 0 24 24" fill="currentColor" style="opacity:0.5">
-                    <path d="M17.05 13.5c-.16-2.74 2.17-4.07 2.26-4.13-.98-1.44-2.51-1.64-3.05-1.66-1.29-.13-2.53.76-3.19.76-.67 0-1.7-.74-2.79-.72-1.44.02-2.77.84-3.51 2.12-1.5 2.6-.38 6.45 1.07 8.56.72 1.04 1.58 2.21 2.71 2.17 1.07-.04 1.48-.69 2.77-.69 1.29 0 1.67.69 2.77.67 1.15-.02 1.88-1.06 2.59-2.1.82-1.19 1.16-2.35 1.18-2.41-.03-.01-2.24-.87-2.27-3.46zM12.03 5.38c.59-.72.98-1.72.87-2.72-.84.03-1.86.56-2.46 1.29-.54.63-1.02 1.63-.89 2.59.94.06 1.9-.47 2.48-1.16z"/>
-                </svg>
-                Apple
             </button>
             <?php endif; ?>
         </div>
