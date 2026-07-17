6db54d0980e06a00e47400b55a49f244fd35163f
Agente Autonomo ShopVivaliz <agente-autonomo@shopvivaliz.com.br>
2026-07-17 18:50:32 -0300

    auto: sincronizar 2026-07-17 18:50:32

--- _tmp_smtp_test.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/scripts/mailer.php';
-$ok = send_email('fredmourao@gmail.com', 'Teste SMTP Gmail ShopVivaliz (nova senha)', '<p>Teste de autenticacao SMTP via Gmail (producao) com nova senha de app.</p>');
-echo $ok ? "OK\n" : "FALHA\n";
