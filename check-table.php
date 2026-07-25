184a374b5ee3b970b21c8c137396e861cf5aa816
Agente Autonomo ShopVivaliz <agente-autonomo@shopvivaliz.com.br>
2026-07-24 23:20:08 -0300

    auto: sincronizar 2026-07-24 23:20:08

--- check-table.php
+++ check-table.php
@@ -7,7 +7,13 @@ foreach (file($env_file) as $line) {
         define(trim($k), trim($v, " \t\n\r\x0B\"'"));
     }
 }
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$db = new mysqli(
+    defined('DB_HOST') ? DB_HOST : 'localhost',
+    defined('DB_USER') ? DB_USER : 'root',
+    defined('DB_PASS') ? DB_PASS : '',
+    defined('DB_NAME') ? DB_NAME : 'shopvivaliz',
+    defined('DB_PORT') ? DB_PORT : 3306
+);
 $r = $db->query('DESCRIBE products');
 while ($row = $r->fetch_assoc()) {
     echo $row['Field'] . PHP_EOL;
