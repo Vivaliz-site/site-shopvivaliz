1e98aab0f8c2ca1def7171fe9e457ffdd8b38110
Agente Autonomo ShopVivaliz <agente-autonomo@shopvivaliz.com.br>
2026-08-12 18:30:18 -0300

    auto: sincronizar 2026-08-12 18:30:18

---
+++ iniciar-fredwin-mcp.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\site-shopvivaliz
+powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\site-shopvivaliz\scripts\fredwin-remote-bootstrap.ps1"
+exit /b 0
