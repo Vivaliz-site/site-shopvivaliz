253555c8301ccbca43c1ba1bcf2c6e940837b159
Frederico mourao <fredmourao@gmail.com>
2026-09-07 21:29:49 -0300

    fix: persist Fred-Win relay watchdog cadence

--- scripts/fredwin-remote-bootstrap.ps1
+++ scripts/fredwin-remote-bootstrap.ps1
@@ -69,7 +69,7 @@ function Install-Task {
     $arguments = '-NoProfile -NonInteractive -ExecutionPolicy Bypass -WindowStyle Hidden -File "' + $script + '" -Mode Ensure'
     $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $arguments -WorkingDirectory $Repo
     $startup = New-ScheduledTaskTrigger -AtStartup
-    $watchdog = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration (New-TimeSpan -Days 3650)
+    $watchdog = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(5) -RepetitionInterval (New-TimeSpan -Minutes 5) -RepetitionDuration (New-TimeSpan -Days 3650)
     $principal = New-ScheduledTaskPrincipal -UserId $user -LogonType S4U -RunLevel Highest
     $settings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RestartCount 999 -RestartInterval (New-TimeSpan -Minutes 1)
     $settings.Hidden = $true
