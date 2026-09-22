294ef4289ea9e97abf761f8f10e233614675d0e1
Frederico mourao <fredmourao@gmail.com>
2026-09-22 11:34:54 -0300

    test: forbid generic Amazon support env sourcing

--- tests/amazon-support-reply-oci-breakglass-contract-test.php
+++ tests/amazon-support-reply-oci-breakglass-contract-test.php
@@ -52,6 +52,8 @@ $forbidden=[
   'SELLER_SUPPORT_OPEN',
   'create new case',
   'open new case',
+  '. /home/ubuntu/amazon-returns-deploy/shared/.env',
+  'tail -20 /tmp/shopvivaliz-support-probe.out',
 ];
 foreach($forbidden as $needle){if(strpos($text,$needle)!==false){fwrite(STDERR,"amazon support Bastion breakglass contains forbidden pattern: {$needle}\n");exit(1);}}
 
@@ -63,6 +65,8 @@ $scriptRequired=[
   'seller-central-support-lookup-probe.mjs',
   'SUPPORT_LOOKUP_PROBE=PASS',
   'SUPPORT_PROBE_RESULT=',
+  'SUPPORT_PROBE_OUTPUT_SHA256=',
+  'PROBE_NO_JSON_OUTPUT',
   'SUPPORT_AUTH_CHECK_FAILED',
   'viewCaseMetaData?.canEditCase===true',
   'TERMINAL_NOT_EDITABLE',
